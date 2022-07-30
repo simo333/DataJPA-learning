9822eee7399d44b1acbe245c7bbb392fd40d14f4
simo333 <szymon.333@wp.pl>
2022-07-30 08:57:57 +0200

    Exceptions handlers

---
+++ src/main/java/com/simo333/datajpalearning/exception/ApiException.java
@@ -0,0 +1,30 @@
+package com.simo333.datajpalearning.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class ApiException {
+    private final String message;
+    private final HttpStatus httpStatus;
+    private final ZonedDateTime timeStamp;
+
+    public ApiException(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+        this.timeStamp = ZonedDateTime.now(ZoneId.systemDefault());
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public ZonedDateTime getTimeStamp() {
+        return timeStamp;
+    }
+}
---
+++ src/main/java/com/simo333/datajpalearning/exception/ApiRequestException.java
@@ -0,0 +1,22 @@
+package com.simo333.datajpalearning.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiRequestException extends RuntimeException {
+    private final HttpStatus httpStatus;
+
+    public ApiRequestException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public ApiRequestException(String message, Throwable cause, HttpStatus httpStatus) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+}
